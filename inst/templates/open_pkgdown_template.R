b18cfa855493ad375fa1eeaaffa5e0286d153086
statnmap <statnmap@users.noreply.github.com>
2019-07-04 15:42:10 +0200

    create gh-pages branch content as travis failed

--- inst/templates/open_pkgdown_template.R
+++
@@ -1,5 +0,0 @@
-#' Open pkgdown site of the package
-#'
-#' @importFrom utils browseURL
-#'
-#' @export
--- inst/templates/open_userguide_template.R
+++
@@ -1,7 +0,0 @@
-#' Open UserGuide of the package
-#'
-#' @param ext extension of the book to open: 'html', 'pdf'
-#'
-#' @importFrom utils browseURL
-#'
-#' @export
