178ee69e635a4a1ae82db30be92032c3678f6d9e
Antoine Bichat <a.bichat@yahoo.fr>
2019-07-19 11:23:18 +0200

    Change default path in open_pkgdown_function

--- R/create_pkgdown.R
+++ R/create_pkgdown.R
@@ -3,7 +3,7 @@
 #' @param path Path to the pkgdown inside your package
 #'
 #' @export
-open_pkgdown_function <- function(path = "docs") {
+open_pkgdown_function <- function(path = "inst/docs") {
 
   pkg_name <- read.dcf("DESCRIPTION")[1,"Package"]
   fun_template <- system.file("templates/open_pkgdown_template.R", package = "chameleon")
